182578aa31cee90d27284f82bea82534ace6bcdd
See Shell <323seeshell@gmail.com>
2020-05-10 18:55:40 -0700

    font brass mono imported and updated for table

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import "./fonts/Nervous.ttf"
+import "./fonts/regular_comfortable.otf"
 
 ReactDOM.render(
   <React.StrictMode>
