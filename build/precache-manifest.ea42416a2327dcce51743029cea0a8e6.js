c97c2a02d5d632419ed15388dc1c0eb6cb19ee91
See Shell <323seeshell@gmail.com>
2020-05-10 19:09:36 -0700

    text aligned for search and subtitle

---
+++ build/precache-manifest.ea42416a2327dcce51743029cea0a8e6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63b06673dcc1cc9079fedc072b1e34f",
+    "url": "/earthquakes-today/index.html"
+  },
+  {
+    "revision": "9667e3ecc42bc6b25416",
+    "url": "/earthquakes-today/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "6927e59739d05c73d7a2",
+    "url": "/earthquakes-today/static/css/main.f0b6ff25.chunk.css"
+  },
+  {
+    "revision": "9667e3ecc42bc6b25416",
+    "url": "/earthquakes-today/static/js/2.d3930ddf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/earthquakes-today/static/js/2.d3930ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6927e59739d05c73d7a2",
+    "url": "/earthquakes-today/static/js/main.957afd01.chunk.js"
+  },
+  {
+    "revision": "51ba83219f0c568422df",
+    "url": "/earthquakes-today/static/js/runtime-main.60d72f16.js"
+  },
+  {
+    "revision": "c32b6cffcd8c065865ffc390d1a568a3",
+    "url": "/earthquakes-today/static/media/earthquake_banner_v_small.c32b6cff.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/earthquakes-today/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9590bd12cea2ba61bddde7efde17970",
+    "url": "/earthquakes-today/static/media/regular_comfortable.c9590bd1.otf"
+  }
+]);
--- src/components/Container.js
+++ src/components/Container.js
@@ -4,6 +4,7 @@ import "./Container.css";
 function Container(props) {
   return (
     <div className="container">
+      
       <div >
           <label className="search">
             search by place:{" "}
@@ -12,6 +13,7 @@ function Container(props) {
               value={props.search}
               onChange={props.handleSearchChange}
             />
+            <p className="subtitle">starting on today's date (UTC)</p>
           </label>
         </div>
         <div className={`container${props.fluid ? "-fluid" : ""}`}>
